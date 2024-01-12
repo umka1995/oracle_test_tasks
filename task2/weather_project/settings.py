38148b7b8cca2589b7f749cbdacdec81e0115420
umka1995 <umka.key@gmail.com>
2024-01-12 19:22:44 +0600

    docker

--- task2/weather_project/settings.py
+++ task2/weather_project/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+# weather_api_project/settings.py
+
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
